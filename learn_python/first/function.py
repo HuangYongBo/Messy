9f7ccf3e43758115996633a495417cf1337d9318
HuangYongBo <18320399334@163.com>
2017-10-17 02:45:33 -0700

    add some file

---
+++ learn_python/first/function.py
@@ -0,0 +1,7 @@
+
+sum=100
+def print_int(a):
+	sum = a;
+	print("print_int",sum);
+
+print_int(10);
---
+++ learn_python/first/input.py
@@ -0,0 +1,7 @@
+def input_int(n):
+	i = 0
+	for i in range(n):
+		j =int(input())
+		print('The %d number is :%d '%(i,j))
+		i = i + 1
+input_int(4)
